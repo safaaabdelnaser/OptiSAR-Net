13be866d93b4c49326f15965a767c323ca688e6c
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-05-05 13:44:07 +0300

    update in number of epoch to be 50 in seaship

--- train.py
+++ train.py
@@ -4,5 +4,5 @@ from ultralytics import YOLO
 
 model = YOLO("OptiSAR-Net.yaml")
 
-model.train(data="seaship.yaml", epochs=100, batch=8, imgsz=416)
+model.train(data="seaship.yaml", epochs=50, batch=8, imgsz=416)
 
