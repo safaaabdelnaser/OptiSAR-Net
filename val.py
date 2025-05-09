01a2521e56860576a252982d278acb7724c95aa8
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-05-09 15:44:56 +0300

    update to open sar

--- val.py
+++ val.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # val seaship 
-model = YOLO('best-seaship.pt')
+# model = YOLO('best-seaship.pt')
 
-model.val(data='seaship.yaml', batch=1)
+# model.val(data='seaship.yaml', batch=1)
 
 # val opensar dataset
-# model = YOLO('/kaggle/working/open-4/runs/detect/train/weights/best.pt')
-# model.val(data='opensar.yaml', batch=8)
+model = YOLO('/kaggle/working/open-4/runs/detect/train/weights/best.pt')
+model.val(data='opensar.yaml', batch=8)
