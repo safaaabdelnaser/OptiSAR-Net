9401d6edc2bdbbe10b9050f533c33afc87d2b60d
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-05-11 00:09:30 +0300

    update

--- train.py
+++ train.py
@@ -8,6 +8,6 @@ model = YOLO("OptiSAR-Net.yaml")
 # model.train(data="seaship.yaml", epochs=50, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 # # opensar dataset
-model = YOLO('OptiSAR-Net.yaml').load('last.pt')
-model.train(data="opensar.yaml", epochs=50 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
+model = YOLO('OptiSAR-Net.yaml').load('last125epochOpensar.pt')
+model.train(data="opensar.yaml", epochs=15 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
