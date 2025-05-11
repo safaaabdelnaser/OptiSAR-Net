90136206331bfef9fdb7074234eba282530959b6
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-05-12 01:46:53 +0300

    update num of epoch

--- train.py
+++ train.py
@@ -9,5 +9,5 @@ model = YOLO("OptiSAR-Net.yaml")
 
 # # opensar dataset
 model = YOLO('OptiSAR-Net.yaml').load('last125epochOpensar.pt')
-model.train(data="opensar.yaml", epochs=15 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
+model.train(data="opensar.yaml", epochs=25 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
