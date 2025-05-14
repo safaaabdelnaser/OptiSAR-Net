a3f1d4ce50c9dbccab5f71744f88152b2571ed2a
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-05-14 23:41:08 +0300

    UPDATE OF shipRIS

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ model = YOLO("OptiSAR-Net.yaml")
 # model.train(data="seaship.yaml", epochs=50, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 # # opensar dataset
-model = YOLO('OptiSAR-Net.yaml').load('last125epochOpensar.pt')
-model.train(data="opensar.yaml", epochs=25 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
+# model = YOLO('OptiSAR-Net.yaml').load('last125epochOpensar.pt')
+# model.train(data="opensar.yaml", epochs=25 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
+# train of shipRIS
+model.train(data="shipRIS.yaml", epochs=50, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
--- val.py
+++ val.py
@@ -6,5 +6,5 @@ from ultralytics import YOLO
 # model.val(data='seaship.yaml', batch=1)
 
 # val opensar dataset
-model = YOLO('best-OPENSAR.pt')
+model = YOLO('/kaggle/working/ShipRSI-1/runs/detect/train/weights/best.pt')
 model.val(data='opensar.yaml', batch=8)
