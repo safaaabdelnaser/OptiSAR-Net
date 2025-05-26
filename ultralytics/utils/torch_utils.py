269e348a460cdd1a6a69b1612a370789a6fbd64f
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-05-26 14:16:01 +0300

    update

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -484,7 +484,9 @@ def strip_optimizer(f: Union[str, Path] = "best.pt", s: str = "") -> None:
         ```
     """
     # x = torch.load(f, map_location=torch.device("cpu"))
-    x = torch.load(f, map_location=torch.device("cpu"), weights_only=False)
+    with torch.serialization.safe_globals(["ultralytics.nn.tasks.DetectionModel"]):
+    x = torch.load(f, map_location=torch.device("cpu"))
+
     if "model" not in x:
         LOGGER.info(f"Skipping {f}, not a valid Ultralytics model.")
         return
