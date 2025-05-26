0941268b42b8454269d0a1b4e23bcee741386154
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-05-26 18:26:15 +0300

    num of epochs

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ model = YOLO("OptiSAR-Net.yaml")
 # model.train(data="opensar.yaml", epochs=25 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 # train of shipRIS
-model.train(data="shipRIS.yaml", epochs=2, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
+model.train(data="shipRIS.yaml", epochs=100, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 
 #train
