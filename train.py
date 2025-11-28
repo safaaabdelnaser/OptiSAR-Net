55406c811cf52c2f39f2f40d52caeda8062a372a
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-11-28 18:59:10 +0200

    fix:update in stracture of yolov8

--- train.py
+++ train.py
@@ -2,7 +2,8 @@
 
 from ultralytics import YOLO
 
-model = YOLO("OptiSAR-Net.yaml")
+# model = YOLO("OptiSAR-Net.yaml")
+model = YOLO("optiSar-Net-p2.yaml")
 # train of seaship
 # model.train(data="seaship.yaml", epochs=50, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
@@ -11,7 +12,7 @@ model = YOLO("OptiSAR-Net.yaml")
 # model.train(data="opensar.yaml", epochs=25 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 # train of shipRIS
-model.train(data="shipRIS.yaml", epochs=100, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
+model.train(data="shipRIS.yaml", epochs=10, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 
 #train
