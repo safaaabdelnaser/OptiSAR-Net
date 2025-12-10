c657d919eab1b847559f0716595969e465b22409
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2025-12-10 20:24:34 +0200

    update

--- train.py
+++ train.py
@@ -5,14 +5,14 @@ from ultralytics import YOLO
 # model = YOLO("OptiSAR-Net.yaml")
 model = YOLO("optiSar-Net-p2.yaml")
 # train of seaship
-# model.train(data="seaship.yaml", epochs=50, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
+ model.train(data="seaship.yaml", epochs=100, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 # # opensar dataset
 # model = YOLO('OptiSAR-Net.yaml').load('last125epochOpensar.pt')
 # model.train(data="opensar.yaml", epochs=25 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 # train of shipRIS
-model.train(data="shipRIS.yaml", epochs=100, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
+#model.train(data="shipRIS.yaml", epochs=100, imgsz=416,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
 
 
 #train
