ae34b7eca80c0d1ce078e7e0cf61ea1d3b4b40b9
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2026-01-11 18:31:31 +0200

    update in train model

--- train.py
+++ train.py
@@ -5,7 +5,31 @@ from ultralytics import YOLO
 # model = YOLO("OptiSAR-Net.yaml")
 model = YOLO("optiSar-Net-p2.yaml")
 # train of seaship
-model.train(data="seaship.yaml", epochs=50, imgsz=416,batch=8,optimizer="Adamax",lr0=3e-4,lrf=0.01,plots=True,val=True,save=True,close_mosaic=0)
+# model.train(data="seaship.yaml", epochs=50, imgsz=416,batch=8,optimizer="Adamax",lr0=3e-4,lrf=0.01,plots=True,val=True,save=True,close_mosaic=0)
+model.train(
+    data="seaship.yaml",
+    epochs=50,
+    imgsz=416,
+    batch=8,
+    optimizer="Adamax",
+    lr0=3e-4,
+    lrf=0.01,
+    box=7.5,
+    cls=0.5,
+    dfl=1.5,
+    obj=1.5,
+    mosaic=0.5,
+    close_mosaic=10,
+    multi_scale=True,
+    fliplr=0.5,
+    scale=0.5,
+    conf=0.001,
+    iou=0.6,
+    plots=True,
+    val=True,
+    save=True
+)
+
 # # opensar dataset
 # model = YOLO('OptiSAR-Net.yaml').load('last125epochOpensar.pt')
 # model.train(data="opensar.yaml", epochs=25 ,imgsz=416 ,plots=True ,val=True ,save=True, batch=8, close_mosaic=0)
